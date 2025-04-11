66394bed804b706cdbdcceee6fd9d95ff6ffe868
mfiferna <57713253+mfiferna@users.noreply.github.com>
2025-04-11 18:26:56 +0200

    Create GrinderMulligan.java

---
+++ forge-game/src/main/java/forge/game/mulligan/GrinderMulligan.java
@@ -0,0 +1,55 @@
+package forge.game.mulligan;
+
+import forge.game.card.CardCollection;
+import forge.game.card.Card;
+import forge.game.zone.ZoneType;
+
+private class GrinderMulligan extends AbstractMulligan {
+    public GrinderMulligan(Player p, boolean firstMullFree) {
+        super(p, firstMullFree);
+    }
+
+    @Override
+    public boolean canMulligan() {
+        return !kept && timesMulliganed < player.getMaxHandSize();
+    }
+
+    @Override
+    public int handSizeAfterNextMulligan() {
+        return player.getMaxHandSize();
+    }
+
+    @Override
+    public void mulligan() {
+        CardCollection toMulligan = new CardCollection(player.getCardsIn(ZoneType.Hand));
+        if (toMulligan.isEmpty()) return;
+        revealPreMulligan(toMulligan);
+        for (final Card c : toMulligan) {
+            player.getGame().getAction().moveToLibrary(c, null);
+        }
+        try {
+            Thread.sleep(100); //delay for a tiny bit to give UI a chance catch up
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        player.shuffle(null);
+        timesMulliganed++;
+        mulliganDraw();
+        player.onMulliganned();
+    }
+
+    @Override
+    public void mulliganDraw() {
+        player.drawCards(handSizeAfterNextMulligan());
+        int tuckingCards = tuckCardsAfterKeepHand();
+
+        for (final Card c : player.getController().londonMulliganReturnCards(player, tuckingCards)) {
+            player.getGame().getAction().moveToLibrary(c, -1, null);
+        }
+    }
+
+    @Override
+    public int tuckCardsAfterKeepHand() {
+        return 2 + timesMulliganed;
+    }
+}
